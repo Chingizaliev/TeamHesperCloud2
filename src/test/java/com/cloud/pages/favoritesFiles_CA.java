1a79ab743ee59fb451d14fdd2fb414f50a29be2b
chingizaliev <chingiz_geo@yahoo.com>
2023-03-05 23:06:07 -0500

    feature files

--- src/test/java/com/cloud/pages/favoritesFiles_CA.java
+++ src/test/java/com/cloud/pages/favoritesFiles_CA.java
@@ -1,6 +1,8 @@
 package com.cloud.pages;
 
 import com.cloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class favoritesFiles_CA {
@@ -10,6 +12,14 @@ public class favoritesFiles_CA {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+@FindBy(xpath = "//span[.='Files']")
+public WebElement files;
+
+ @FindBy(xpath = "//a[@class='action action-menu permanent']")
+ public WebElement favorites;
+
+ @FindBy(xpath="//span[.='Add to favorites']")
+ public WebElement addToFavorites;
 
 
 }
